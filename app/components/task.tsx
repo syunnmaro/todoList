7df1c853c22dc3b5376e1b59bb740db880748c33
haruku <haruku.oka@st-hakky.com>
2024-09-26 19:55:44 +0900

    implemented api

--- app/components/task.tsx
+++ app/components/task.tsx
@@ -1,27 +1,42 @@
-import {TaskType} from "@/app/page";
+"use client"
+import { Prisma, Todo } from '@prisma/client';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircle, faCircleCheck} from "@fortawesome/free-solid-svg-icons";
 import React, {useEffect, useState} from "react";
+import {updateTaskType} from "@/app/page";
 
 export const Task = ({initialTask, updateTaskList}: {
-    initialTask: TaskType,
-    updateTaskList: (task: TaskType) => void
+    initialTask: Todo,
+    updateTaskList: (task: Todo) => void
 }) => {
-    const [task, setTask] = useState<TaskType>(initialTask);
-
-    const reverseStatus = () => {
-        setTask(prevTask => ({
-            ...prevTask,
-            isDone: !prevTask.isDone
-        }));
+    const [task, setTask] = useState<Todo>(initialTask);
+    const updateData =async (task: updateTaskType) => {
+        await fetch("http://localhost:3000/api/tasks", {
+            cache: "no-cache", method: "PUT", body: JSON.stringify({
+                id: task.id,
+                title: task.title,
+                isDone:task.isDone
+            })
+        });
+    }
+    const reverseDoneStatus = () => {
+        const newTask = {
+            ...task,
+            isDone: !task.isDone
+        }
+        setTask(newTask);
+        updateData(task);
+        return newTask
     };
 
-    const updateTitle = (e: React.FormEvent<HTMLInputElement>) => {
+    const updateTitleStatus = (e: React.FormEvent<HTMLInputElement>) => {
         const input = e.target as HTMLInputElement;
-        setTask(prevTask => ({
-            ...prevTask,
+        const newTask = {
+            ...task,
             title: input.value
-        }));
+        }
+        setTask(newTask);
+        return newTask
     };
 
     useEffect(() => {
@@ -35,15 +50,15 @@ export const Task = ({initialTask, updateTaskList}: {
             <th className="">
                 <div className="border-2 hover:border-gray-300 border-white p-2">
                     {task.isDone
-                        ? <FontAwesomeIcon icon={faCircleCheck} onClick={reverseStatus}/>
-                        : <FontAwesomeIcon icon={faCircle} onClick={reverseStatus}/>
+                        ? <FontAwesomeIcon icon={faCircleCheck} onClick={reverseDoneStatus}/>
+                        : <FontAwesomeIcon icon={faCircle} onClick={reverseDoneStatus}/>
                     }
                 </div>
             </th>
             <td className="h-full">
                 <div className="border-2 hover:border-gray-300 border-white focus:shadow-2xl h-12 outline-none">
-                    <input value={task.title} onInput={(e) => updateTitle(e)}
-                           className={`h-full outline-none ${task.isDone ? "line-through decoration-2" : ""}`}/>
+                    <input value={task.title} onInput={(e) => updateTitleStatus(e)}
+                           className={`h-full outline-none ${task.isDone ? "line-through decoration-2" : ""}`} onBlur={()=>updateData(task)}/>
                 </div>
             </td>
         </tr>
